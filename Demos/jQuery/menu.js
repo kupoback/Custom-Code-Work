05b76e4fa154480f009bda7434d21ff1e747e0c4
Nick <nick@rhinogroup.com>
2015-11-27 14:27:30 -0600

    Fixed issues with SASS to get it working.

---
+++ Demos/jQuery/menu.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('nav ul li > a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+});
