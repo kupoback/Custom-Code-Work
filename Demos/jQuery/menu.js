4e38db4b1d15359e62507e0379db802a8c4152c8
Nick Makris <makris@me.com>
2015-11-30 08:48:02 -0600

    Worked on the navigation and fixed up issues with SCSS

--- Demos/jQuery/menu.js
+++ Demos/jQuery/menu.js
@@ -1,10 +1,4 @@
 $(function() {
-  $('nav ul li > a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').toggle();
-      $('.nav-dropdown').not($(this).siblings()).hide();
-      e.stopPropagation();
-    });
-    $('html').click(function() {
-      $('.nav-dropdown').hide();
-    });
+
+
 });
