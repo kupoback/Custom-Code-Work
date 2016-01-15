2cdd16af58fbe29be5f8c3dd90553cdd11468e2a
Nick <nick@rhinogroup.com>
2016-01-15 16:59:57 -0600

    Added comments to the code I was working on.

--- Angular.js/Lesson 2 CodeAcademy/js/directives/appInfo.js
+++ Angular.js/Lesson 2 CodeAcademy/js/directives/appInfo.js
@@ -1,9 +1,12 @@
 app.directive('appInfo', function() {
   return {
-    restrict: 'E',
+    restrict: 'E', // Specifices How Directive will be used, 'E' = HTML element
     scope: {
-      info: '='
+      info: '=' // Specifies scope will pass information into directive through attr
+                // named info. '=' means to look for attr named 'info' in the <app-info> element
+                // that we created in this function.
     },
-    templateUrl: 'js/directives/appInfo.html'
+    templateUrl: 'js/directives/appInfo.html' // Tells code what HTML file is to be used to display
+                                              // the content gained by $scope.info
   };
 });
