1b78a0765219010f22c14610a7bf2ced8b282c45
Nick <nick@rhinogroup.com>
2016-01-22 15:22:18 -0600

    Got through one step in the Plugin Tutorial

--- Demos/WP Plugins/admin-panel/includes/class-admin-panel.php
+++ Demos/WP Plugins/admin-panel/includes/class-admin-panel.php
@@ -176,6 +176,13 @@ class Admin_Panel {
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
+		// Add Menu item
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_plugin_admin_menu' );
+
+		// Add Settings link to the plugin
+		$plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
+		$this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_action_links' );
+
 	}
 
 	/**
