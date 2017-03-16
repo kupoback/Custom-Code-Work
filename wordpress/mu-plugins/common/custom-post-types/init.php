bf4f77cb247f6fdcde13ac8e55767f9ffa7571a5
kupoback <makris@me.com>
2017-03-16 14:16:33 -0500

    Create init.php

---
+++ wordpress/mu-plugins/common/custom-post-types/init.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *  Loads required Custom Post Types
+ *  
+ *  @since 1.0.0
+ *  
+ *  @package orgCommunity
+ *  @subpackage Admin
+ *  @since 1.0.0
+ */
+
+
+/**
+ *  Post Type: People
+ *  
+ *  @since 1.0.0
+ */
+include_once(dirname(__FILE__) . '/cpt-testimonials.php');
+
+
+/**
+ *  Post Type: Call to Action
+ *  
+ *  @since 1.0.0
+ */
+include_once(dirname(__FILE__) . '/cpt-button.php');
