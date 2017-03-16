b5a29344f296da7c60842fbef6f8ce6fa685215d
kupoback <makris@me.com>
2017-03-16 14:11:52 -0500

    Create init.php

---
+++ wordpress/mu-plugins/common/shortcodes/init.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *  Loads required Shortcodes
+ *  
+ *  @since 1.0.0
+ *  
+ *  @package Custom
+ *  @subpackage Admin
+ *  @since 1.0.0
+ */
+
+
+/**
+ *  Shortcode: Call to Action
+ *  
+ *  @since 1.0.0
+ */
+include_once(dirname(__FILE__) . '/call-to-action/load.php');
+
+/**
+ *  Shortcode: CPT Button
+ *
+ *  @since 1.0.0
+ */
+include_once(dirname(__FILE__) . '/cpt-button/load.php');
+
+/**
+ *  Shortcode: CPT Testimonial
+ *
+ *  @since 1.0.0
+ */
+include_once(dirname(__FILE__) . '/testimonial/load.php');
